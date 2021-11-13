f40067a3f7b8d01a77a385a16ccb4a5032f5ee34
Marcel T.O <marcel@turobin-ort.at>
2021-11-13 14:10:20 +0100

    Finished command tests.

---
+++ SplayTree/UnitTest/Commands/DisplayCommand_Test.cs
@@ -0,0 +1,37 @@
+namespace UnitTest.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using SplayTree.Commands;
+    using SplayTree.Exceptions;
+    using SplayTree.Interfaces;
+    using SplayTree.Logic;
+    using SplayTree.Trees;
+
+    [TestClass]
+    public class DisplayCommand_Test
+    {
+        [TestMethod]
+        [ExpectedException(typeof(TreeIsEmptyException))]
+        public  void TestIfTreeIsEmpty()
+        {
+            SplayTree_int splaytree = new SplayTree_int(new List<Node>());
+            DisplayCommand command = new DisplayCommand(splaytree);
+
+            command.Execute();
+        }
+
+        [TestMethod]
+        public  void TestIfTreeGetsCleared()
+        {
+            ILogger logger = new ConsoleLogger();
+            Executioner execute = new Executioner(logger);
+
+            SplayTree_int splaytree = new SplayTree_int(execute.GenerateTree(new List<int> {7,9,3,6,5}));
+            DisplayCommand command = new DisplayCommand(splaytree);
+
+            command.Execute();
+        }
+    }
+}
