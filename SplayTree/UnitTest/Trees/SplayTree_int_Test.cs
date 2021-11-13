e1a149a49e14a425eb582009c5a635166b62130a
Marcel T.O <marcel@turobin-ort.at>
2021-11-13 14:19:20 +0100

    Added splay tree tests.

---
+++ SplayTree/UnitTest/Trees/SplayTree_int_Test.cs
@@ -0,0 +1,52 @@
+﻿namespace UnitTest.Trees
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using SplayTree.Logic;
+    using SplayTree.Trees;
+
+    [TestClass]
+    public class SplayTree_int_Test
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestIfConstructorIsNull()
+        {
+            SplayTree_int splaytree = new SplayTree_int(null);
+        }
+
+        [TestMethod]
+        public void TestConstructor()
+        {
+            SplayTree_int splaytree = new SplayTree_int(new List<Node>());
+
+            Assert.IsNotNull(splaytree);
+        }
+
+        [TestMethod]
+        public void TestGetCommands()
+        {
+            SplayTree_int splaytree = new SplayTree_int(new List<Node>());
+
+            Assert.IsNotNull(splaytree.Commands);
+        }
+
+        [TestMethod]
+        public void TestNodesGet()
+        {
+            SplayTree_int splaytree = new SplayTree_int(new List<Node>());
+
+            Assert.IsNotNull(splaytree.Nodes);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestNodesSet()
+        {
+            SplayTree_int splaytree = new SplayTree_int(new List<Node>());
+
+            splaytree.Nodes = null;
+        }
+    }
+}
