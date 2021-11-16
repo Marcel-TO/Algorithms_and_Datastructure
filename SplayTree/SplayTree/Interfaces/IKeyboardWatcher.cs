558e488db48b728539f4619e1dbca1d2ba4e1770
Marcel T.O <marcel@turobin-ort.at>
2021-11-16 14:07:59 +0100

    Added interface for keyboard watcher.

---
+++ SplayTree/SplayTree/Interfaces/IKeyboardWatcher.cs
@@ -0,0 +1,12 @@
+﻿namespace SplayTree.Interfaces
+{
+    using System;
+    using SplayTree.KeyboardWatcher;
+
+    public interface IKeyboardWatcher
+    {
+        public event EventHandler<KeyboardWatcherKeyPressedEventArgs> KeyPressed;
+
+        void Start();
+    }
+}
