af2d33d61a7d2f535aa0b33ac4eb0fb3e6377214
Marcel T.O <marcel@turobin-ort.at>
2021-11-16 14:08:53 +0100

    Added instances for console logger and keyboard watcher. - They are replacing the current for testing purposes.

---
+++ SplayTree/UnitTest/Replacements/ConsoleLoggerTestInstance.cs
@@ -0,0 +1,99 @@
+﻿namespace UnitTest.Replacements
+{
+    using System;
+    using SplayTree.Commands;
+    using SplayTree.Interfaces;
+
+    public class ConsoleLoggerTestInstance : ILogger
+    {
+        public int ChooseTraverseOrder()
+        {
+            return 1;
+        }
+
+        public void Clear()
+        {
+            return;
+        }
+
+        public void Continue()
+        {
+            return;
+        }
+
+        public int GetValueFromUser()
+        {
+            return 3;
+        }
+
+        public void Message(string message)
+        {
+            return;
+        }
+
+        public void ShowCommands(BaseCommand[] commands)
+        {
+            return;
+        }
+
+        public void ShowCursor(int index, int lengthOfCommandList)
+        {
+            return;
+        }
+
+        public void Visit(InsertCommand command)
+        {
+            return;
+        }
+
+        public void Visit(RemoveCommand command)
+        {
+            return;
+        }
+
+        public void Visit(ClearCommand command)
+        {
+            return;
+        }
+
+        public void Visit(CountCommand command)
+        {
+            return;
+        }
+
+        public void Visit(CountSpecificCommand command)
+        {
+            return;
+        }
+
+        public void Visit(MinCommand command)
+        {
+            return;
+        }
+
+        public void Visit(MaxCommand command)
+        {
+            return;
+        }
+
+        public void Visit(ContainsCommand command)
+        {
+            return;
+        }
+
+        public void Visit(TraverseCommand command)
+        {
+            return;
+        }
+
+        public void Visit(DisplayCommand command)
+        {
+            return;
+        }
+
+        public void WelcomeMessage()
+        {
+            return;
+        }
+    }
+}
---
+++ SplayTree/UnitTest/Replacements/KeyboardWatcherTestInstance.cs
@@ -0,0 +1,19 @@
+﻿namespace UnitTest.Replacements
+{
+    using System;
+    using SplayTree.Interfaces;
+    using SplayTree.KeyboardWatcher;
+
+    public class KeyboardWatcherTestInstance : IKeyboardWatcher
+    {
+        public event EventHandler<KeyboardWatcherKeyPressedEventArgs> KeyPressed;
+
+        public void Start()
+        {
+            this.KeyPressed?.Invoke(this, new KeyboardWatcherKeyPressedEventArgs(ConsoleKey.UpArrow));
+            this.KeyPressed?.Invoke(this, new KeyboardWatcherKeyPressedEventArgs(ConsoleKey.DownArrow));
+            this.KeyPressed?.Invoke(this, new KeyboardWatcherKeyPressedEventArgs(ConsoleKey.Enter));
+            this.KeyPressed?.Invoke(this, new KeyboardWatcherKeyPressedEventArgs(ConsoleKey.Escape));
+        }
+    }
+}
