f1b68073e3e4174fa6040dbc81a357faf9e527ef
Marcel-TO <marcel@turobin-ort.at>
2021-11-17 10:36:26 +0100

    Added style cop for trees directory.

--- SplayTree/SplayTree/Trees/SplayTree_int.cs
+++ SplayTree/SplayTree/Trees/SplayTree_int.cs
@@ -1,4 +1,10 @@
-﻿
+﻿//-----------------------------------------------------------------------
+// <copyright file="SplayTree_int.cs" company="FHWN">
+//     Copyright (c) Marcel Turobin-Ort. All rights reserved.
+// </copyright>
+// <author>Marcel Turobin-Ort</author>
+// <summary>Defines the splay tree containing integer values.</summary>
+//-----------------------------------------------------------------------
 namespace SplayTree.Trees
 {
     using System;
@@ -6,12 +12,26 @@ namespace SplayTree.Trees
     using SplayTree.Commands;
     using SplayTree.Logic;
 
+    /// <summary>
+    /// Represents the integer splay tree.
+    /// </summary>
     public class SplayTree_int
     {
+
+        /// <summary>
+        /// Represents the list of all nodes.
+        /// </summary>
         private List<Node> nodes;
 
+        /// <summary>
+        /// Represents the list of all supported commands.
+        /// </summary>
         private BaseCommand[] commands;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SplayTree_int"/> class.
+        /// </summary>
+        /// <param name="nodes">Represents the list of all nodes.</param>
         public SplayTree_int(List<Node> nodes)
         {
             this.Nodes = nodes;
@@ -31,6 +51,10 @@ namespace SplayTree.Trees
             };
         }
 
+        /// <summary>
+        /// Gets or sets the list of all nodes.
+        /// </summary>
+        /// <value>The list of all nodes.</value>
         public List<Node> Nodes
         {
             get
@@ -49,11 +73,25 @@ namespace SplayTree.Trees
             }
         }
 
+        /// <summary>
+        /// Gets the list of all command.
+        /// </summary>
+        /// <value>The list of all command.</value>
         public BaseCommand[] Commands
         {
             get
             {
                 return this.commands;
+            }
+
+            private set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException($"The {nameof(this.commands)} must not be null.");
+                }
+
+                this.commands = value;
             }
         }
     }
