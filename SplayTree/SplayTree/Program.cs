dac3237b769ac66b763ad52eb15604bd5e58169c
Marcel-TO <marcel@turobin-ort.at>
2021-11-17 10:39:00 +0100

    Added style cop to program.

--- SplayTree/SplayTree/Program.cs
+++ SplayTree/SplayTree/Program.cs
@@ -1,12 +1,25 @@
-﻿namespace SplayTree
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="FHWN">
+//     Copyright (c) Marcel Turobin-Ort. All rights reserved.
+// </copyright>
+// <author>Marcel Turobin-Ort</author>
+// <summary>Defines the program of the application.</summary>
+//-----------------------------------------------------------------------
+namespace SplayTree
 {
-    using System;
     using System.Diagnostics.CodeAnalysis;
     using SplayTree.Logic;
 
+    /// <summary>
+    /// Represents the program start of the application.
+    /// </summary>
     [ExcludeFromCodeCoverage]
     public class Program
     {
+        /// <summary>
+        /// Represents the main method to start the application.
+        /// </summary>
+        /// <param name="args">Represents the command line arguments.</param>
         public static void Main(string[] args)
         {
             ApplicationLogic application = new ApplicationLogic(new ConsoleLogger(), new KeyboardWatcher.KeyboardWatcher());
