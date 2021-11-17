e2f2662272de7064b3ee9ee20b368b20c13dc94b
Marcel-TO <marcel@turobin-ort.at>
2021-11-17 08:38:01 +0100

    Added style cop to the keyboard watcher directory.

--- SplayTree/SplayTree/KeyboardWatcher/KeyboardWatcherKeyPressedEventArgs.cs
+++ SplayTree/SplayTree/KeyboardWatcher/KeyboardWatcherKeyPressedEventArgs.cs
@@ -1,18 +1,25 @@
-﻿namespace SplayTree.KeyboardWatcher
+﻿//-----------------------------------------------------------------------
+// <copyright file="KeyboardWatcherKeyPressedEventArgs.cs" company="FHWN">
+//     Copyright (c) Marcel Turobin-Ort. All rights reserved.
+// </copyright>
+// <author>Marcel Turobin-Ort</author>
+// <summary>Defines the arguments of the keyboard watcher event.</summary>
+//-----------------------------------------------------------------------
+namespace SplayTree.KeyboardWatcher
 {
     using System;
     using System.Diagnostics.CodeAnalysis;
 
     /// <summary>
-    /// This Class holds the KeyboardWatcher Data.
+    /// Represents the arguments of the event.
     /// </summary>
     [ExcludeFromCodeCoverage]
     public class KeyboardWatcherKeyPressedEventArgs : EventArgs
     {
         /// <summary>
-        /// Initializes a new instance of the KeyboardWatcherKeyPressedEventArgs class.
+        /// Initializes a new instance of the <see cref="KeyboardWatcherKeyPressedEventArgs"/> class.
         /// </summary>
-        /// <param name="key"> The pressed Key.</param>
+        /// <param name="key">Represents key pressed by user.</param>
         public KeyboardWatcherKeyPressedEventArgs(ConsoleKey key)
         {
             this.Key = key;
