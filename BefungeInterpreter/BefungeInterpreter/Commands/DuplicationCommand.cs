acda23441379a2bd7886b2fff4afc798dfb567ae
Marcel T.O <marcel@turobin-ort.at>
2021-11-21 14:01:54 +0100

    Fixed duplication command.

--- BefungeInterpreter/BefungeInterpreter/Commands/DuplicationCommand.cs
+++ BefungeInterpreter/BefungeInterpreter/Commands/DuplicationCommand.cs
@@ -17,6 +17,12 @@
 
         public void Execute(BefungeProgram program)
         {
+            if (program.Stack.Count == 0)
+            {
+                program.Stack.Push(0);
+                program.ValueList.Add(0);
+            }
+
             int value = program.Stack.Peek();
             program.Stack.Push(value);
             program.ValueList.Add(value);
